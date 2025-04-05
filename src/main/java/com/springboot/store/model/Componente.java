a7836ed5ff8d7ba499dea3736da101de27f9875d
nrodr <nrodriguesalves05@gmail.com>
2025-04-05 05:13:21 -0300

    feat(model): cria classe base abstrata Componente

---
+++ src/main/java/com/springboot/store/model/Componente.java
@@ -0,0 +1,47 @@
+package com.springboot.store.model;
+
+public class Componente {
+    private Long id;
+    private String nome;
+    private int consumo;
+    private double  preco;
+
+    public Componente(Long id, String nome, int consumo, double preco){
+        this.id = id;
+        this.nome = nome;
+        this.consumo = consumo;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(int consumo) {
+        this.consumo = consumo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
